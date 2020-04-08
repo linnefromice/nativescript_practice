8482d936f7ec3bb13f9a2b43025762e4e6b3aae5
linnefromice <linnefromice@gmail.com>
2020-04-08 22:26:06 +0900

    add navigation logic

--- tutorial_multipage/app/main-view-model.js
+++ tutorial_multipage/app/main-view-model.js
@@ -18,6 +18,12 @@ function createViewModel() {
         viewModel.set("message", getMessage(viewModel.counter));
     };
 
+    viewModel.navigate = (args) => {
+        const button = args.object;
+        const page = button.page;
+        page.frame.navigate("beta");
+    }
+
     return viewModel;
 }
 

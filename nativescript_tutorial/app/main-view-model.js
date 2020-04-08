98ce3a85a8d87e255f24437dfef69c44689a0c0d
linnefromice <linnefromice@gmail.com>
2020-04-08 20:55:15 +0900

    modify logic (add & minus counter)

--- nativescript_tutorial/app/main-view-model.js
+++ nativescript_tutorial/app/main-view-model.js
@@ -1,27 +1,21 @@
 const Observable = require("tns-core-modules/data/observable").Observable;
 
 function getMessage(counter) {
-    if (counter <= 0) {
-        return "Hoorraaay! You unlocked the NativeScript clicker achievement!";
-    } else {
-        return `${counter} taps left`;
-    }
+    return `${counter}`;
 }
 
 function createViewModel() {
     const viewModel = new Observable();
-    viewModel.counter = 42;
-    viewModel.message = getMessage(viewModel.counter);
+    let counter = 10;
+    viewModel.message = getMessage(counter);
 
-    viewModel.onTap = () => {
-        viewModel.counter--;
-        viewModel.set("message", getMessage(viewModel.counter));
-    };
     viewModel.requestAdd = () => {
-        console.log('REQUEST PLUS');
+        counter++;
+        viewModel.set("message", getMessage(counter));
     }
     viewModel.requestMinus = () => {
-        console.log('REQUEST MINUS');
+        counter--;
+        viewModel.set("message", getMessage(counter));
     }
     return viewModel;
 }
